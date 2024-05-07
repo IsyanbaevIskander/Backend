26a98b4469430de0f60042498af8a28636bea7b3
IsyanbaevIskander <125815570+IsyanbaevIskander@users.noreply.github.com>
2024-05-07 16:21:16 +0500

    Delete core directory

--- core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from core import models
-
-
-admin.site.register(models.Subject)
-admin.site.register(models.Teacher)
-admin.site.register(models.Parent)
-admin.site.register(models.Group)
-admin.site.register(models.Student)
