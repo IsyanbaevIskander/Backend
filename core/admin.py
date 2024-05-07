c8fc2e971e8cae9a00aad99fdda4a58cb783aa94
IsyanbaevIskander <125815570+IsyanbaevIskander@users.noreply.github.com>
2024-05-07 16:17:06 +0500

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -3,6 +3,7 @@ from core import models
 
 
 admin.site.register(models.Subject)
-admin.site.register(models.Student)
 admin.site.register(models.Teacher)
 admin.site.register(models.Parent)
+admin.site.register(models.Group)
+admin.site.register(models.Student)
