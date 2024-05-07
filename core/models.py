d6f4d3d0b9e69222db347ee56487805866419a41
IsyanbaevIskander <125815570+IsyanbaevIskander@users.noreply.github.com>
2024-05-07 16:16:34 +0500

    Update models.py

--- core/models.py
+++ core/models.py
@@ -3,58 +3,24 @@ from datetime import date
 
 
 class Subject(models.Model):
-    subjects = [('Иностранный язык', 'Иностранный язык'),
-                ('Математика', 'Математика'),
-                ('Русский язык', 'Русский язык'),
-                ('История', 'История'),
-                ('Обществознание', 'Обществознание'),
-                ('Физическая культура', 'Физическая культура'),
-                ('ОБЖ', 'ОБЖ'),
-                ('Физика', 'Физика'),
-                ('Химия', 'Химия'),
-                ('Биология', 'Биология'),
-                ('География', 'География'),
-                ('Информатика', 'Информатика'),
-                ]
-    complexities = [('Базовый', 'Базовый уровень'),
-                    ('Продвинутый', 'Продвинутый уровень')]
-    subjects.sort()
-    name = models.CharField(choices=subjects, max_length=30)
-    complexity = models.CharField(choices=complexities, max_length=30)
-    members = models.ManyToManyField('Student')
+    name = models.CharField('Название', max_length=30)
 
     class Meta:
         verbose_name = 'Предмет'
         verbose_name_plural = 'Предметы'
 
     def __str__(self) -> str:
-        return f'{self.name}, {self.complexity} уровень'
-
-
-class Student(models.Model):
-    name = models.CharField(default='', max_length=20)
-    surname = models.CharField(default='', max_length=20)
-    middlename = models.CharField(default='', max_length=20)
-    date_of_birth = models.DateField(default=date.today(), blank=False)
-
-    grades = [(i, f'{i} класс') for i in range(1, 12)]
-    grade = models.PositiveIntegerField(default=1, choices=grades)
-    parents = models.ManyToManyField('Parent')
-
-    class Meta:
-        verbose_name = 'Ученик'
-        verbose_name_plural = 'Ученики'
-
-    def __str__(self) -> str:
-        return f'{self.name} {self.surname}'
+        return f'{self.name}'
 
 
 class Teacher(models.Model):
-    name = models.CharField(default='', max_length=20)
-    surname = models.CharField(default='', max_length=20)
-    middlename = models.CharField(default='', max_length=20)
-    date_of_birth = models.DateField(default=date.today(), blank=False)
-    subjects = models.ManyToManyField('Subject')
+    name = models.CharField('Имя', max_length=20)
+    surname = models.CharField('Фамилия', max_length=20)
+    middlename = models.CharField('Отчество', max_length=20, blank=True)
+    date_of_birth = models.DateField('Дата рождения', default=date.today())
+    sex = models.CharField('Пол', max_length=1, choices=(('М', 'М'), ('Ж', 'Ж')), default='М')
+    photo = models.ImageField('Фотография', upload_to='pictures', null=True, blank=True)
+    subjects = models.ManyToManyField(Subject, verbose_name='Преподаваемые предметы')
 
     class Meta:
         verbose_name = 'Учитель'
@@ -65,9 +31,11 @@ class Teacher(models.Model):
 
 
 class Parent(models.Model):
-    name = models.CharField(default='', max_length=20)
-    surname = models.CharField(default='', max_length=20)
-    middlename = models.CharField(default='', max_length=20)
+    name = models.CharField('Имя', max_length=20)
+    surname = models.CharField('Фамилия', max_length=20)
+    middlename = models.CharField('Отчество', max_length=20, blank=True)
+    sex = models.CharField('Пол', max_length=1, choices=(('М', 'М'), ('Ж', 'Ж')), default='М')
+    photo = models.ImageField('Фотография',upload_to='pictures', blank=True)
 
     class Meta:
         verbose_name = 'Родитель'
@@ -75,3 +43,34 @@ class Parent(models.Model):
 
     def __str__(self):
         return f'{self.surname} {self.name} {self.middlename}'
+
+
+class Group(models.Model):
+    number = models.PositiveIntegerField('Номер', choices=tuple((i, i) for i in range(1, 12)), default=1)
+    letter = models.CharField('Буква', max_length=1, default='')
+    subjects = models.ManyToManyField(Subject, verbose_name='Изучаемые предметы')
+
+    class Meta:
+        verbose_name = 'Класс'
+        verbose_name_plural = 'Классы'
+
+    def __str__(self):
+        return f'{self.number} {self.letter} класс'
+
+
+class Student(models.Model):
+    name = models.CharField('Имя',  max_length=20)
+    surname = models.CharField('Фамилия', max_length=20)
+    middlename = models.CharField('Отчество', max_length=20, blank=True)
+    date_of_birth = models.DateField('Дата рождения', default=date.today(), blank=False)
+    sex = models.CharField('Пол', max_length=1, choices=(('М', 'М'), ('Ж', 'Ж')), default='М')
+    parents = models.ManyToManyField(Parent, verbose_name='Родители')
+    photo = models.ImageField('Фотография', upload_to='pictures', blank=True)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, default='', verbose_name='Класс')
+
+    class Meta:
+        verbose_name = 'Ученик'
+        verbose_name_plural = 'Ученики'
+
+    def __str__(self) -> str:
+        return f'{self.name} {self.surname}'
